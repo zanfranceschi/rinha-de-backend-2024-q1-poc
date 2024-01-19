75a0ee3a63a8b1e80dc8b10c02b74b707ce59064
Francisco Zanfranceschi <zanfranceschi@gmail.com>
2024-01-19 02:18:48 -0300

    clean up

--- src/rinha_2024q1_crebito/http_handlers.clj
+++ src/rinha_2024q1_crebito/http_handlers.clj
@@ -6,15 +6,16 @@
 
 (def tipos-transacao #{"c" "d"})
 
-;; https://camdez.com/blog/2015/08/27/practical-data-coercion-with-prismatic-schema/
+;; - 2.147.483.647 é o valor máximo para INTEGER do Postgres
+;; - Com créditos demais, pode haver erros de out of range.
+;;   (Não validado propositalmente)
 (s/defschema Transacao
-  {:valor     s/Int
+  {:valor     (s/pred #(and (pos-int? %) (<= % 1000000))) 
    :tipo      (apply s/enum tipos-transacao)
    :descricao (s/pred (fn [d]
                         (and (string? d)
                              (and (<= (count d) 10)
                                   (>= (count d) 1)))))})
-
 (defn extrato!
   [{db-spec :db-spec
     {cliente_id* :id} :route-params
@@ -48,82 +49,70 @@
 
 (defn creditar!
   [cliente_id clientes valor descricao db-spec]
-  (try
-    (jdbc/with-transaction [conn db-spec]
-      (jdbc/execute! conn ["select pg_advisory_xact_lock(?)" cliente_id])
-      (jdbc/execute! conn ["insert into transacoes
-                            (id, cliente_id, valor, tipo, descricao, realizada_em) 
-                            values(default, ?, ?, 'c', ?, now())"
-                           cliente_id
-                           valor
-                           descricao])
-      (let [{novo-saldo :saldos/saldo} (jdbc/execute-one! conn ["update saldos
-                                                                 set valor = valor + ?
-                                                                 where cliente_id = ?
-                                                                 returning valor as saldo"
-                                                                valor
-                                                                cliente_id])
-            {limite :limite} (get clientes cliente_id)]
-        {:status 200
-         :body {:limite limite
-                :saldo novo-saldo}}))
-    (catch Exception e
-      (if (string/includes? (.getMessage e) "violates foreign key constraint \"fk_clientes_transacoes_id\"")
-        {:status 404}
-        (throw e)))))
+  (jdbc/with-transaction [conn db-spec]
+    (jdbc/execute! conn ["select pg_advisory_xact_lock(?)" cliente_id])
+    (jdbc/execute! conn ["insert into transacoes
+                          (id, cliente_id, valor, tipo, descricao, realizada_em) 
+                          values(default, ?, ?, 'c', ?, now())"
+                         cliente_id
+                         valor
+                         descricao])
+    (let [{novo-saldo :saldos/saldo} (jdbc/execute-one! conn ["update saldos
+                                                               set valor = valor + ?
+                                                               where cliente_id = ?
+                                                               returning valor as saldo"
+                                                              valor
+                                                              cliente_id])
+          {limite :limite} (get clientes cliente_id)]
+      {:status 200
+       :body {:limite limite
+              :saldo novo-saldo}})))
 
 (defn debitar!
   [cliente_id clientes valor descricao db-spec]
-  (try
-    (jdbc/with-transaction [conn db-spec]
-      (jdbc/execute-one! conn ["select pg_advisory_xact_lock(?)" cliente_id])
-      (let [{limite :limite} (get clientes cliente_id)
-            {saldo :saldos/saldo} (jdbc/execute-one! conn ["select valor as saldo
-                                                            from saldos
-                                                            where cliente_id = ?"
-                                                           cliente_id])
-            ultrapassaria-limite? (< (- saldo valor) (* limite -1))]
-        (if ultrapassaria-limite?
-          {:status 422
-           :body {:erro "limite insuficiente"}}
-          (let [{novo-saldo :saldos/saldo} (jdbc/execute-one! conn ["update saldos
-                                                                     set valor = valor + ?
-                                                                     where cliente_id = ?
-                                                                     returning valor as saldo"
-                                                                    (* valor -1)
-                                                                    cliente_id])]
-            (jdbc/execute! conn ["insert into transacoes
-                                  (id, cliente_id, valor, tipo, descricao, realizada_em)
-                                  values(default, ?, ?, 'd', ?, now())"
-                                 cliente_id
-                                 valor
-                                 descricao])
-            {:status 200
-             :body {:limite limite
-                    :saldo novo-saldo}}))))
-    (catch Exception e
-      (if (string/includes? (.getMessage e) "violates foreign key constraint \"fk_clientes_transacoes_id\"")
-        {:status 404}
-        (throw e)))))
+  (jdbc/with-transaction [conn db-spec]
+    (jdbc/execute-one! conn ["select pg_advisory_xact_lock(?)" cliente_id])
+    (let [{limite :limite} (get clientes cliente_id)
+          {saldo :saldos/saldo} (jdbc/execute-one! conn ["select valor as saldo
+                                                          from saldos
+                                                          where cliente_id = ?"
+                                                         cliente_id])
+          ultrapassaria-limite? (< (- saldo valor) (* limite -1))]
+      (if ultrapassaria-limite?
+        {:status 422
+         :body {:erro "limite insuficiente"}}
+        (let [{novo-saldo :saldos/saldo} (jdbc/execute-one! conn ["update saldos
+                                                                   set valor = valor + ?
+                                                                   where cliente_id = ?
+                                                                   returning valor as saldo"
+                                                                  (* valor -1)
+                                                                  cliente_id])]
+          (jdbc/execute! conn ["insert into transacoes
+                                (id, cliente_id, valor, tipo, descricao, realizada_em)
+                                values(default, ?, ?, 'd', ?, now())"
+                               cliente_id
+                               valor
+                               descricao])
+          {:status 200
+           :body {:limite limite
+                  :saldo novo-saldo}})))))
 
 (defn transacionar!
   [{db-spec :db-spec payload :body {cliente_id* :id} :route-params clientes :cached-clientes}]
-  (try
-    (let [cliente_id (Integer/parseInt cliente_id*)
-          {valor     :valor
-           tipo      :tipo
-           descricao :descricao} (s/validate Transacao payload)]
-      (cond
-        (or (< valor 1)
-            (nil? (some #{tipo} #{"c" "d"}))) {:status 422
-                                               :body   {:erro "Valor precisa ser maior que 0 e tipo 'c' ou 'd'."}}
-        (= tipo "c") (creditar! cliente_id clientes valor descricao db-spec)
-        (= tipo "d") (debitar! cliente_id clientes valor descricao db-spec)))
-    (catch Exception e
-      (if (= :schema.core/error (:type (ex-data e)))
-        {:status 422
-         :body   {:erro "manda essa merda direito com 'valor', 'tipo' e 'descricao'"}}
-        (throw e)))))
+  (if-let [{cliente_id :id} (get clientes (Integer/parseInt cliente_id*))]
+    (try
+      (let [{valor     :valor
+             tipo      :tipo
+             descricao :descricao} (s/validate Transacao payload)]
+        (cond
+          (= tipo "c") (creditar! cliente_id clientes valor descricao db-spec)
+          (= tipo "d") (debitar! cliente_id clientes valor descricao db-spec)))
+      (catch Exception e
+        (if (= :schema.core/error (:type (ex-data e)))
+          {:status 422
+           :body   {:erro "manda essa merda direito com 'valor', 'tipo' e 'descricao'"}}
+          (throw e))))
+    {:status 404}))
 
 (defn clientes!
   [{db-spec :db-spec}]
