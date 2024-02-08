86bc01b6b8bb3fd1df735c04998567141033c74f
Francisco Zanfranceschi <zanfranceschi@gmail.com>
2024-02-07 22:29:31 -0300

    ..

--- containerization/run.sh
+++ containerization/run.sh
@@ -2,5 +2,5 @@
 
 # exemplo de uso: `./run.sh nginx`
 
-docker-compose -f ./$1/docker-compose.yml down
-docker-compose -f ./$1/docker-compose.yml up
+docker compose -f ./$1/docker-compose.yml down
+docker compose -f ./$1/docker-compose.yml up
